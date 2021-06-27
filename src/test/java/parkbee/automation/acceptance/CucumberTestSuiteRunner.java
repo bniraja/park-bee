ec90b4a1d001132d0fd4a713fd9a614b42142555
Neeraja b <b.niraja@gmail.com>
2021-06-27 17:04:02 +0200

    serenity configuration changes

--- src/test/java/parkbee/automation/acceptance/CucumberTestSuiteRunner.java
+++ src/test/java/parkbee/automation/acceptance/CucumberTestSuiteRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = "src/test/resources/features/ui",
+        features = "src/test/resources/features",
         glue = {"parkbee.automation"}
 )
 public class CucumberTestSuiteRunner {
